43302a25b0b29c393c74f1a968c2a446318b34fd
Swaraj Gaikwad <116364064+swaraj017@users.noreply.github.com>
2025-08-02 13:23:34 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "http://localhost:5000/api"
+const API_BASE_URL = "https://lms-rtq5.onrender.com"
 
 // Create axios instance
 const api = axios.create({
