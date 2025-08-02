56132f6ff6af73d60d7ad2221132d941dd9e57a0
swaraj017 <swaraj017@gmail.com>
2025-08-02 13:18:37 +0530

    Cleaned up project, added .gitignore

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
 const { body, validationResult } = require("express-validator")
-const User = require("../models/User")
+const User = require("../models/user")
 const auth = require("../middleware/auth")
 
 const router = express.Router()
